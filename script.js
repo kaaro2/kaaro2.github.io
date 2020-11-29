21831ed02ca2193f43957abc2b30b77f368c1623
kaaro2 <74704843+kaaro2@users.noreply.github.com>
2020-11-29 23:02:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function function1() {
+  document.getElementById("dropdown1").classList.toggle("show1");
+}
+function function2() {
+  document.getElementById("dropdown2").classList.toggle("show2");
+}
