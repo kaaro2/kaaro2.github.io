d919c3edc39d898a7c828d2a7958855311863992
kaaro2 <74704843+kaaro2@users.noreply.github.com>
2020-11-29 23:07:40 +0200

    Delete script.js

--- Pizza/script.js
+++
@@ -1,6 +0,0 @@
-function function1() {
-  document.getElementById("dropdown1").classList.toggle("show1");
-}
-function function2() {
-  document.getElementById("dropdown2").classList.toggle("show2");
-}
